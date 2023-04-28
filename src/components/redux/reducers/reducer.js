8b2cfe0ae4832a57a02c4d7d7a89334c39a59a72
Utkarshdatta <utkarshdatta5@gmail.com>
2023-04-28 13:04:24 +0530

    reducer functionality

--- src/components/redux/reducers/reducer.js
+++ src/components/redux/reducers/reducer.js
@@ -5,17 +5,45 @@ const INIT_STATE = {
 export const cartreducer = (state = INIT_STATE, action) => {
     switch (action.type) {
         case "ADD_CART":
+
+        const ItemIndex = state.carts.findIndex((item)=> item.id === action.payload.id);
+
+        if (ItemIndex >=0){
+            state.carts[ItemIndex].qnty +=1
+        }else{
+            const temp = {...action.payload, qnty:1}
+        
             return {
                 ...state,
-                carts: [...state.carts, action.payload]
+                carts: [...state.carts, temp]
             }
+        }
+
+
+
             case "RMV_CART":
                 const data = state.carts.filter((el)=>el.id !== action.payload);
                 return {
                     ...state,
                     carts:data
                 }
+
+            case "RMV_ONE":
+                const ItemIndex_dec = state.carts.findIndex((item)=> item.id === action.payload.id);
+
+                if(state.carts[ItemIndex_dec].qnty >= 1 ){
+                    const dltitem = state.carts[ItemIndex_dec].qnty -= 1
+
+                    return{
+                        ...state,
+                        carts:[...state.carts]
+                    }
+
+                }
+
+
         default:
             return state
     }
 }
+
